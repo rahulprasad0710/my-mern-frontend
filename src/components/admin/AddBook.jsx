8e884f9c99a17aeb63759f9a24c303745aea2bf8
rahulprasad0710 <raulshah2021@hotmail.com>
2021-05-04 21:18:45 +0530

    refactor image book upload

--- src/components/admin/AddBook.jsx
+++ src/components/admin/AddBook.jsx
@@ -1,21 +1,23 @@
 import "./addbook.css";
+import "react-toastify/dist/ReactToastify.css";
 
-import React, { useContext, useState } from "react";
-
-import { FaBook } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const AddBook = () => {
     const [fileInput, setFileInput] = useState("");
     const [previewSource, setPreviewSource] = useState(null);
     const [values, setvalues] = useState({
-        name: "",
-        performers: "",
-        venue: "",
+        title: "",
+        publication: "",
+        quantityInStock: "",
+        mrpRate: "",
+        subject: "",
+        level: "",
+        author: "",
+        edition: "",
+        priceRate: "",
         description: "",
-        date: "",
-        time: "",
-        address: "",
     });
 
     const handleInputChange = (e) => {
@@ -24,6 +26,27 @@ const AddBook = () => {
     };
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (!previewSource) {
+            toast.error("Image is not uploaded");
+            return;
+        }
+        const checkEmptyFields = Object.values(values).some(
+            (element) => element === ""
+        );
+        if (checkEmptyFields) {
+            toast.error("All fields should be filleds ");
+            return;
+        } else {
+            const res = await fetch("http://localhost:5000/admin/upload", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ imgData: previewSource, values }),
+            });
+
+            console.log(res);
+        }
     };
 
     const handleChangeImg = (e) => {
@@ -39,33 +62,11 @@ const AddBook = () => {
             setPreviewSource(reader.result);
         };
     };
-    const handleSubmitImage = (e) => {
-        e.preventDefault();
-        console.log("working 1");
-        if (!previewSource) return;
-        console.log("working");
-        uploadImageFn(previewSource);
-    };
-    const uploadImageFn = async (base64EncodedImage) => {
-        console.log(base64EncodedImage);
-        try {
-            const imgResponse = await fetch(
-                "http://localhost:5000/admin/upload",
-                {
-                    method: "POST",
-                    body: JSON.stringify({ imgData: base64EncodedImage }),
-                    headers: { "Content-Type": "application/json" },
-                }
-            );
-            console.log(imgResponse);
-        } catch (error) {
-            console.log(error);
-        }
-    };
 
     return (
         <div>
             <h2>Add a book</h2>
+            <ToastContainer position="top-center" />
             <div className="forms">
                 <form
                     className="book-form"
@@ -192,11 +193,10 @@ const AddBook = () => {
                             rows="10"
                         />{" "}
                     </div>
-                </form>
 
-                <form className="image-form" onSubmit={handleSubmitImage}>
+                    <div className="form-image"></div>
                     <div className="form-group">
-                        <label htmlFor="passwordAgain">Image</label>
+                        <label>Image</label>
                         <input
                             type="file"
                             placeholder="Image"
@@ -204,9 +204,6 @@ const AddBook = () => {
                             className="form-control"
                             value={fileInput}
                         />
-                        <button onSubmit={handleSubmitImage} type="submit">
-                            Upload
-                        </button>
                     </div>
 
                     <div className="form-group">
